6a04277077640fe03fde9eef97aeb195ed4f72ab
FFDeroca16 <ffderoca@gmail.com>
2026-08-14 14:41:21 +0800

    Added stg_lineitem and modified stg_customer

--- snowflake_dbt_transformation_pipeline/models/stg/stg_customer.sql
+++ snowflake_dbt_transformation_pipeline/models/stg/stg_customer.sql
@@ -1,6 +1,13 @@
 WITH customer AS (
     SELECT 
-        *
+        c_custkey AS cust_key,
+        c_name AS name,
+        c_address AS address,
+        c_nationkey AS nation_key,
+        c_phone AS phone,
+        c_acctbal AS acct_bal,
+        c_mktsegment AS mkt_segment, 
+        c_comment AS comment
     FROM 
         {{ source('raw', 'customer') }}
 )
---
+++ snowflake_dbt_transformation_pipeline/models/stg/stg_lineitem.sql
@@ -0,0 +1,26 @@
+WITH lineitem AS (
+    SELECT 
+        l_orderkey AS order_key,
+        l_partkey AS part_key,
+        l_suppkey AS supp_key,
+        l_linenumber AS line_number,
+        l_quantity AS quantity,
+        l_extendedprice AS extended_price,
+        l_discount AS discount,
+        l_tax AS tax,
+        l_returnflag AS return_flag,
+        l_linestatus AS line_status,
+        l_shipdate AS ship_date, 
+        l_commitdate AS commit_date,
+        l_receiptdate AS receipt_date,
+        l_shipinstruct AS ship_instruct,
+        l_shipmode AS shop_mode,
+        l_comment AS comment
+    FROM 
+        {{ source('tpch', 'lineitem') }}
+)
+
+SELECT
+    *
+FROM 
+    lineitem
